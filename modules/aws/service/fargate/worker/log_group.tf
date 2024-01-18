d3b751951eaa81065c7f0181be3af2b74d78eca9
snkk1210 <n41210guitar@gmail.com>
2024-01-18 18:38:42 +0900

    fix

--- modules/aws/service/fargate/worker/log_group.tf
+++ modules/aws/service/fargate/worker/log_group.tf
@@ -3,7 +3,7 @@
 */
 
 // ECS
-resource "aws_cloudwatch_log_group" "log" {
+resource "aws_cloudwatch_log_group" "this" {
   name              = "/ecs/${var.common.project}-${var.common.environment}-${var.common.service_name}"
   retention_in_days = var.ecs_log_retention_in_days
 
