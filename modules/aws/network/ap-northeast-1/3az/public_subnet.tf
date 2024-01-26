2b74be5df433009d3ab0689d850cd5e4cc3a4ffe
snkk1210 <n41210guitar@gmail.com>
2024-01-26 21:46:10 +0900

    modify

---
+++ modules/aws/network/ap-northeast-1/3az/public_subnet.tf
@@ -0,0 +1,73 @@
+/**
+# Public Sunbet
+*/
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.common.id
+  cidr_block              = "${var.cidr_prefix}.0.0/22"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+  tags = {
+    Name = "${var.common.project}-${var.common.environment}-public-subnet-1a${var.sfx}"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id                  = aws_vpc.common.id
+  cidr_block              = "${var.cidr_prefix}.4.0/22"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1c"
+  tags = {
+    Name = "${var.common.project}-${var.common.environment}-public-subnet-1c${var.sfx}"
+  }
+}
+
+resource "aws_subnet" "public_1d" {
+  vpc_id                  = aws_vpc.common.id
+  cidr_block              = "${var.cidr_prefix}.8.0/22"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1d"
+  tags = {
+    Name = "${var.common.project}-${var.common.environment}-public-subnet-1d${var.sfx}"
+  }
+}
+
+/**
+# Internet Gateway
+*/
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.common.id
+  tags = {
+    Name = "${var.common.project}-${var.common.environment}-igw${var.sfx}"
+  }
+}
+
+/** 
+# Route Table ( Public )
+*/
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.common.id
+  tags = {
+    Name = "${var.common.project}-${var.common.environment}-public-rt${var.sfx}"
+  }
+}
+
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  gateway_id             = aws_internet_gateway.this.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_1c" {
+  subnet_id      = aws_subnet.public_1c.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_1d" {
+  subnet_id      = aws_subnet.public_1d.id
+  route_table_id = aws_route_table.public_route_table.id
+}
