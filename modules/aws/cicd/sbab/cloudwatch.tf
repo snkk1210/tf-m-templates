544cf93596c52f45edbae44305f493fc52ee32ee
snkk1210 <n41210guitar@gmail.com>
2024-05-01 22:06:25 +0900

    del: gb sbab

--- modules/aws/cicd/sbab/cloudwatch.tf
+++
@@ -1,7 +0,0 @@
-/**
-# CodeBuild Log
-*/
-resource "aws_cloudwatch_log_group" "codebuild" {
-  name              = "/codebuild/${var.common.environment}/${var.common.service_name}"
-  retention_in_days = 400
-}
